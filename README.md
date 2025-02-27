4f1bb47131b7b130490ac4a164629581f0041f6b
José Comparotto <jose.q.gomes@caixa.gov.br>
2025-02-27 15:15:19 -0300

    feat: Adicionado suporte à subtração

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Interpretador de expressões matemáticas para aplicar as teorias de Linguagens 
 ## Gramática
 
 ```ebnf
-<input> ::= <sum-expression>
+<input> ::= <additive-expression>
 
-<sum-expression> ::= <sum-expression>, <sum-operator>, <number> | <number>;
+<additive-expression> ::= <additive-expression>, <additive-operator>, <number> | <number>;
 
 <number> ::= <integer-number> | <floating-number>;
 
@@ -17,7 +17,7 @@ Interpretador de expressões matemáticas para aplicar as teorias de Linguagens 
 
 <digit> ::= '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
 <decimal-separator> ::= '.';
-<sum-operator> ::= '+';
+<additive-operator> ::= '+' | '-';
 
 (* Notação EBNF - ISO/IEC 14977:1996 <https://www.iso.org/standard/26153.html> *)
 ```
--- __main__.py
+++ __main__.py
@@ -1,17 +1,22 @@
 #!/bin/env python
 import re
 
-NUMBER_PATTERN = r'\d+(?:\.\d+)?'
-SUM_EXP_PATTERN = rf'((?:{NUMBER_PATTERN}\+)*{NUMBER_PATTERN})\+({NUMBER_PATTERN})'
+NUMBER = r'(?:\d+(?:\.\d+)?)'   # No groups
+ADDITIVE_OPE = r'[+-]'          # No groups
+ADDITIVE_EXP = rf'(?:((?:{NUMBER}{ADDITIVE_OPE})*{NUMBER})({ADDITIVE_OPE})({NUMBER}))'
+    # Groups: (left-number-or-expression, operator, right-number)
 
 def math_eval(expression: str):
 
-    if re.match(rf'^{NUMBER_PATTERN}$', expression):
+    if re.match(rf'^{NUMBER}$', expression):
         return float(expression)
 
-    m = re.match(rf'^{SUM_EXP_PATTERN}$', expression)
+    m = re.match(rf'^{ADDITIVE_EXP}$', expression)
     if m:
-        return math_eval(m[1]) + float(m[2])
+        if m[2] == '+':
+            return math_eval(m[1]) + float(m[3])
+        elif m[2] == '-':
+            return math_eval(m[1]) - float(m[3])
 
 while True:
     expression = input("> ")
