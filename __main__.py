50bc11002e6d17dc713478f39244f62f58de8886
José Comparotto <jose.q.gomes@caixa.gov.br>
2025-02-28 14:51:26 -0300

    feat: Adicionado suporte à multiplicação e subtração
    Completando as 4 operações básicas

--- __main__.py
+++ __main__.py
@@ -1,27 +1,65 @@
 #!/bin/env python
 import re
 
-NUMBER = r'(?:\d+(?:\.\d+)?)'   # No groups
-ADDITIVE_OPE = r'[+-]'          # No groups
-ADDITIVE_EXP = rf'(?:((?:{NUMBER}{ADDITIVE_OPE})*{NUMBER})({ADDITIVE_OPE})({NUMBER}))'
-    # Groups: (left-number-or-expression, operator, right-number)
+NUMBER = r'(?:\d+(?:\.\d+)?)'
+VALID_EXPRESSION = rf'^\s*{NUMBER}\s*(?:[*/+-]{NUMBER}\s*)*$'
+
+def number_solve(expression):
+
+    m = re.match(rf'^\s*({NUMBER})\s*$', expression)
+    if not m:
+        raise ValueError("Expressão inválida! Não foi possivel resolver valor.")
+
+    return float(m[1])
+
+def multiplicative_solve(expression):
+
+    match = re.match(r'^(.+)([*/])(.+?)$', expression)
+    if not match:
+        return number_solve(expression)
+
+    left, ope, right = match.groups()
+
+    left = multiplicative_solve(left)
+    right = number_solve(right)
+   
+    if ope == '*':
+        return left * right
+        
+    if ope == '/':
+        return left / right
+
+def additive_solve(expression):
+
+    match = re.match(r'^(.+)([+-])(.+?)$', expression)
+    if not match:
+        return multiplicative_solve(expression)
+
+    left, ope, right = match.groups()
+
+    left = additive_solve(left)
+    right = multiplicative_solve(right)
+   
+    if ope == '+':
+        return left + right
+        
+    if ope == '-':
+        return left - right
 
 def math_eval(expression: str):
+    
+    if not re.match(VALID_EXPRESSION, expression):
+        raise ValueError(f"Expressão inválida: '{expression}'")
 
-    if re.match(rf'^{NUMBER}$', expression):
-        return float(expression)
+    result = additive_solve(expression)
+    result = int(result) if int(result) == result else result
 
-    m = re.match(rf'^{ADDITIVE_EXP}$', expression)
-    if m:
-        if m[2] == '+':
-            return math_eval(m[1]) + float(m[3])
-        elif m[2] == '-':
-            return math_eval(m[1]) - float(m[3])
+    return result
 
-while True:
-    expression = input("> ")
-    result = math_eval(expression)
-    if int(result) == result:
-        print(int(result))
-    else:
+if __name__ == '__main__':
+    while True:
+        expression = input("> ")
+        
+        result = math_eval(expression)
+
         print(result)
